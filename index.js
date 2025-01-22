5a0fe055b79b03de69d5f7b6ca776105d96c67a4
hasifjalil <80858980+hasifjalil@users.noreply.github.com>
2025-01-22 19:45:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const getTargetDomain = (host, rootDomain) => {
   return host.split(`.${rootDomain}`)[0]; 
 }
  
-const ownDomain = "unofficial.us.kg";
+const ownDomain = "hasifjalil.my";
 
 async function handleRequest(request) {
   const url = new URL(request.url);
